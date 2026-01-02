905168eab8eff66d393a0fadb6b6198451aa31e5
batuhanyilmaz2814 <batuhanyilmaz2814@outlook.com>
2026-01-02 22:07:52 +0300

    testing

--- proje/paneldepon.c
+++ proje/paneldepon.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 char rastgele_olusum();
 void yazdirma(char oyun_alani[20][20], int N, int M);
 
 int main(){
-    srand(time(NULL));
+    srand(42); /*Hayatın, evrenin ve her şeyin anlamı.*/
     int secim = 0;
     int i,j;
     int N,M,tempN,tempM; 
@@ -15,8 +14,10 @@ int main(){
     int D1X,D1Y,D2X,D2Y;
     int PX,PY;
     int ust_satir;
+    int yer_degisikligi = 0;
+    int patlatma_sayisi = 0;
     do{
-    printf("Matris boyutlarını giriniz: \n");
+    printf("Matris boyutlarini giriniz: \n");
     printf("Satir sayisi(En fazla 20): ");
     scanf("%d",&N);
     printf("Sutun sayisi(En fazla 20): ");
@@ -35,7 +36,7 @@ int main(){
 
     for(i=0; i<N; i++){
         for(j=0; j<M; j++){
-            oyun_alani[i][j] = ' ';
+            oyun_alani[i][j] = '.';
         }
     }
 
@@ -46,6 +47,21 @@ int main(){
             }
         }
 
+    }
+    else if(oyun_modu == 2){
+        for(i=N-1; i>=(N/2); i--){
+            for(j=0; j<M; j++){
+                printf("Oyun alaninin %d. satir %d. sutun elemanini giriniz: ",i - (N/2) + 1,j + 1);
+                scanf(" %c",&oyun_alani[i][j]);
+            }
+        }
+        yazdirma(oyun_alani,N,M);
+
+    }else{
+        printf("Hatali oyun modu secimi yaptiniz.\n");
+    }
+
+    if(oyun_modu == 1 || oyun_modu == 2){
         while(secim != 3 && ust_satir != 0){
 
             yazdirma(oyun_alani,N,M);
@@ -55,38 +71,46 @@ int main(){
             if(secim == 1){
                 printf("Yer degistirmek istediğiniz ilk nesnenin satir numarasini girin: ");
                 scanf("%d",&D1X);
-                D1X = D1X + ust_satir;
                 printf("Yer degistirmek istediğiniz ilk nesnenin sutun numarasini girin: ");
                 scanf("%d",&D1Y);
                 printf("Yer degistirmek istediğiniz ikinci nesnenin satir numarasini girin: ");
                 scanf("%d",&D2X);
-                D2X = D2X + ust_satir;
                 printf("Yer degistirmek istediğiniz ikinci nesnenin sutun numarasini girin: ");
                 scanf("%d",&D2Y);
 
-                char temp = oyun_alani[D1X-1][D1Y-1];
-                oyun_alani[D1X-1][D1Y-1] = oyun_alani[D2X-1][D2Y-1];
-                oyun_alani[D2X-1][D2Y-1] = temp;
+                if(oyun_alani[D1X-1][D1Y-1] == '.' || oyun_alani[D2X-1][D2Y-1] == '.'){
+                    printf("Bosluk ile yer degisikligi yapilamaz!\n");
+                } else {
+                    char temp = oyun_alani[D1X-1][D1Y-1];
+                    oyun_alani[D1X-1][D1Y-1] = oyun_alani[D2X-1][D2Y-1];
+                    oyun_alani[D2X-1][D2Y-1] = temp;
 
-                for(i=ust_satir; i<N; i++){
-                for(j=0; j<M; j++){
-                    oyun_alani[i-1][j] = oyun_alani[i][j];
-                }
-            }
-
-            for(j=0; j<M; j++){
-                oyun_alani[N-1][j] = rastgele_olusum();
-            }
-               /* yazdirma(oyun_alani,N,M);*/
-               int satir_kontrol = 0;
-                ust_satir = ust_satir - 1;
-                for(j=0; j<M; j++){
-                    if(oyun_alani[ust_satir][j] == ' '){
-                        satir_kontrol = satir_kontrol + 1;
+                    int satir_kontrol = 0;
+                    for(j=0; j<M; j++){
+                        if(oyun_alani[ust_satir][j] == '.'){
+                            satir_kontrol = satir_kontrol + 1;
+                        }
                     }
-                }
-                if(satir_kontrol == M){
-                    ust_satir = ust_satir + 1;
+                    if(satir_kontrol == M){
+                        ust_satir = ust_satir + 1;
+                    }else{
+                        ust_satir = ust_satir - 1;
+                    }
+                
+
+                    for(i=ust_satir; i<N; i++){
+                    for(j=0; j<M; j++){
+                        oyun_alani[i-1][j] = oyun_alani[i][j];
+                        }
+                    }
+
+                    for(j=0; j<M; j++){
+                        oyun_alani[N-1][j] = rastgele_olusum();
+                    }
+                   /* yazdirma(oyun_alani,N,M);*/
+                    yer_degisikligi = yer_degisikligi + 1;
+                    printf("Yer degisikligi: %d\n",yer_degisikligi);
+                    printf("Patlatma sayisi: %d\n",patlatma_sayisi);
                 }
                 
             }else if(secim == 2){
@@ -95,62 +119,73 @@ int main(){
                 printf("Patlatmak istediginiz grubun sol ust kosesinin sutun numarasini girin: ");
                 scanf("%d",&PY);
 
-                PX = PX + ust_satir;
-
-                char patlatma_karakter = oyun_alani[PX-1][PY-1];
                 
-                int yatay_say = 1;
-                for(int j = PY-2; j >= 0 && oyun_alani[PX-1][j] == patlatma_karakter; j--){
-                    yatay_say++;
-                }
-                for(int j = PY; j < M && oyun_alani[PX-1][j] == patlatma_karakter; j++){
-                    yatay_say++;
-                }
-                
-                int dikey_say = 1;
-                for(int i = PX-2; i >= 0 && oyun_alani[i][PY-1] == patlatma_karakter; i--){
-                    dikey_say++;
-                }
-                for(int i = PX; i < N && oyun_alani[i][PY-1] == patlatma_karakter; i++){
-                    dikey_say++;
-                }
-                
-                if(yatay_say >= 3 || dikey_say >= 3){
-                    if(yatay_say >= dikey_say){
-                        oyun_alani[PX-1][PY-1] = ' ';
-                        for(int j = PY-2; j >= 0 && oyun_alani[PX-1][j] == patlatma_karakter; j--){
-                            oyun_alani[PX-1][j] = ' ';
-                        }
-                        for(int j = PY; j < M && oyun_alani[PX-1][j] == patlatma_karakter; j++){
-                            oyun_alani[PX-1][j] = ' ';
-                        }
-                    } else {
-                        oyun_alani[PX-1][PY-1] = ' ';
-                        for(int i = PX-2; i >= 0 && oyun_alani[i][PY-1] == patlatma_karakter; i--){
-                            oyun_alani[i][PY-1] = ' ';
-                        }
-                        for(int i = PX; i < N && oyun_alani[i][PY-1] == patlatma_karakter; i++){
-                            oyun_alani[i][PY-1] = ' ';
-                        }
+                if(oyun_alani[PX-1][PY-1] == '.'){
+                    printf("Bosluk patlatilamaz!\n");
+                } else {
+                    /*Patlatma yonunun secilmesi*/
+                    char patlatma_karakter = oyun_alani[PX-1][PY-1];
+                    
+                    int yatay_say = 1;
+                    for(j = PY-2; j >= 0 && oyun_alani[PX-1][j] == patlatma_karakter; j--){
+                        yatay_say++;
+                    }
+                    for(j = PY; j < M && oyun_alani[PX-1][j] == patlatma_karakter; j++){
+                        yatay_say++;
                     }
                     
-                    for(int j = 0; j < M; j++){
-                        int yaz = N - 1;
-                        for(int oku = N - 1; oku >= 0; oku--){
-                            if(oyun_alani[oku][j] != ' '){
-                                oyun_alani[yaz][j] = oyun_alani[oku][j];
-                                if(yaz != oku){
-                                    oyun_alani[oku][j] = ' ';
+                    int dikey_say = 1;
+                    for(i = PX-2; i >= 0 && oyun_alani[i][PY-1] == patlatma_karakter; i--){
+                        dikey_say++;
+                    }
+                    for(i = PX; i < N && oyun_alani[i][PY-1] == patlatma_karakter; i++){
+                        dikey_say++;
+                    }
+                    
+                    if(yatay_say >= 3 || dikey_say >= 3){
+                        if(yatay_say >= dikey_say){
+                            oyun_alani[PX-1][PY-1] = '.';
+                            for(j = PY-2; j >= 0 && oyun_alani[PX-1][j] == patlatma_karakter; j--){
+                                oyun_alani[PX-1][j] = '.';
+                            }
+                            for(j = PY; j < M && oyun_alani[PX-1][j] == patlatma_karakter; j++){
+                                oyun_alani[PX-1][j] = '.';
+                            }
+                            patlatma_sayisi = patlatma_sayisi + yatay_say;
+                        } else {
+                            oyun_alani[PX-1][PY-1] = '.';
+                            for(i = PX-2; i >= 0 && oyun_alani[i][PY-1] == patlatma_karakter; i--){
+                                oyun_alani[i][PY-1] = '.';
+                            }
+                            for(i = PX; i < N && oyun_alani[i][PY-1] == patlatma_karakter; i++){
+                                oyun_alani[i][PY-1] = '.';
+                            }
+                            patlatma_sayisi = patlatma_sayisi + dikey_say;
+                        }
+                        /*Yercekimi mekanigi*/
+                        int oku;
+                        int kalan;
+                        for(j = 0; j < M; j++){
+                            int yaz = N - 1;
+                            for(oku = N - 1; oku >= 0; oku--){
+                                if(oyun_alani[oku][j] != '.'){
+                                    oyun_alani[yaz][j] = oyun_alani[oku][j];
+                                    if(yaz != oku){
+                                        oyun_alani[oku][j] = '.';
+                                    }
+                                    yaz--;
                                 }
-                                yaz--;
+                            }
+                            for(kalan = yaz; kalan >= 0; kalan--){
+                                oyun_alani[kalan][j] = '.';
                             }
                         }
-                        for(int kalan = yaz; kalan >= 0; kalan--){
-                            oyun_alani[kalan][j] = ' ';
-                        }
+                        
+                        printf("Yer degisikligi: %d\n",yer_degisikligi);
+                        printf("Patlatma sayisi: %d\n",patlatma_sayisi);
+                    } else {
+                        printf("Bu koordinatta en az 3 ayni tas arka arka degil!\n");
                     }
-                } else {
-                    printf("Bu koordinatta en az 3 aynı tas arka arka degil!\n");
                 }
             }
 
@@ -163,7 +198,6 @@ int main(){
                 printf("Oyun bitti. Tesekkurler!\n");}
             if(ust_satir == 0){
                 printf("Oyun alani doldu. Oyun bitti!\n");}
-
     }
 
 
